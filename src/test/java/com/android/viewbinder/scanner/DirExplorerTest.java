2cbd2c8a49024caa8dcac5222ae28caf4ba77243
ParkJaesung <jspark141515@gmail.com>
2017-04-10 19:31:59 +0900

    Added simple juit test case

---
+++ src/test/java/com/android/viewbinder/scanner/DirExplorerTest.java
@@ -0,0 +1,14 @@
+package com.android.viewbinder.scanner;
+
+import org.junit.Test;
+
+/**
+ * Created by parkjaesung on 2017. 4. 10..
+ */
+public class DirExplorerTest {
+    @Test
+    public void explore() throws Exception {
+
+    }
+
+}
