9390117838aff23dc83a550b0fb634c9b148c9be
ParkJaesung <jspark141515@gmail.com>
2017-04-13 16:17:14 +0900

    Added name member and getter & setter method

--- src/main/java/com/android/viewbinder/unit/FragmentUnit.java
+++ src/main/java/com/android/viewbinder/unit/FragmentUnit.java
@@ -4,4 +4,14 @@ package com.android.viewbinder.unit;
  * Created by parkjaesung on 2017. 4. 13..
  */
 public class FragmentUnit {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
