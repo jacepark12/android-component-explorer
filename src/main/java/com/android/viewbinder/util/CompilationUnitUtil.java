c91d30978d9a34a14f80383bed8733bfb2475797
ParkJaesung <jspark141515@gmail.com>
2017-04-13 16:14:42 +0900

    Created compilationUnitUtil class for DI injection

---
+++ src/main/java/com/android/viewbinder/util/CompilationUnitUtil.java
@@ -0,0 +1,38 @@
+package com.android.viewbinder.util;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseException;
+import com.github.javaparser.ast.CompilationUnit;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by parkjaesung on 2017. 4. 13..
+ */
+public class CompilationUnitUtil {
+
+    private CompilationUnit getCompilationUnit(File file) throws ParseException, FileNotFoundException {
+        FileInputStream in = new FileInputStream(file);
+        CompilationUnit compilationunit = JavaParser.parse(in);
+
+        return compilationunit;
+    }
+
+    @NotNull
+    private ArrayList<String> getClassSourceList(File classFile) throws ParseException, FileNotFoundException {
+        CompilationUnit compilationunit = getCompilationUnit(classFile);
+        String classSource = compilationunit.getTypes().get(0).toString();
+        return new ArrayList<String>(Arrays.asList(classSource.split(" ")));
+    }
+
+    @NotNull
+    private ArrayList<String> getClassSourceList(CompilationUnit compilationunit) throws ParseException, FileNotFoundException {
+        String classSource = compilationunit.getTypes().get(0).toString();
+        return new ArrayList<String>(Arrays.asList(classSource.split(" ")));
+    }
+}
