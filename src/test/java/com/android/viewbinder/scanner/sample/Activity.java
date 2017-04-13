b040f2ffde4876b9e8b2f0acbc8c937545074f3b
ParkJaesung <jspark141515@gmail.com>
2017-04-13 10:39:00 +0900

    Created sample package for testing ClassParser class

---
+++ src/test/java/com/android/viewbinder/scanner/sample/Activity.java
@@ -0,0 +1,11 @@
+package com.android.viewbinder.scanner.sample;
+
+/**
+ * Created by parkjaesung on 2017. 4. 13..
+ */
+public class Activity {
+
+    public void onCreate(){
+
+    }
+}
---
+++ src/test/java/com/android/viewbinder/scanner/sample/SampleClass.java
@@ -0,0 +1,7 @@
+package com.android.viewbinder.scanner.sample;
+
+/**
+ * Created by parkjaesung on 2017. 4. 13..
+ */
+public class SampleClass extends Activity{
+}
