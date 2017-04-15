f9da433144afc1f9e11fdc9f77a1b2d6647c5e1a
ParkJaesung <jspark141515@gmail.com>
2017-04-16 00:37:52 +0900

    Updated FileHandler class

--- src/main/java/com/android/component/explorer/scanner/FileHandler.java
+++ src/main/java/com/android/component/explorer/scanner/FileHandler.java
@@ -1,11 +1,59 @@
 package com.android.component.explorer.scanner;
 
-import java.io.File;
+import com.android.component.explorer.manager.UnitManager;
+import com.github.javaparser.ParseException;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by parkjaesung on 2017. 4. 10..
  */
 public class FileHandler implements DirExplorer.FileHandler {
+
+    private static FileHandler instance = new FileHandler();
+
+    UnitManager unitManager = UnitManager.getInstance();
+    ClassParser classParser = ClassParser.getInstance();
+
+    Set<String> activityClassNames = new HashSet<String>();
+
+    private FileHandler(){
+        try {
+
+            //System.out.println(String.valueOf(getClass().getResourceAsStream("activity_classes.properties")));
+            File file = new File(ClassLoader.getSystemClassLoader().getResource("activity_classes.properties").getPath());
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader((fileReader));
+
+            String line;
+            while((line = bufferedReader.readLine())!= null){
+                activityClassNames.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static FileHandler getInstance(){
+        return instance;
+    }
+
     public void handle(int level, String path, File file) {
+        try {
+            classParser.getParentClassName(file);
+            System.out.println("Analyzed " + path + " Parent class name : " + classParser.getParentClassName(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Set<String> getActivityClassNames() {
+        return activityClassNames;
     }
 }
