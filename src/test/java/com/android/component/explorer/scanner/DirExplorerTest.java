9fd38f94804d626b5a5847044fd35799ddb26fdc
ParkJaesung <jspark141515@gmail.com>
2017-04-16 00:37:21 +0900

    Updated DirExplorerTest

--- src/test/java/com/android/component/explorer/scanner/DirExplorerTest.java
+++ src/test/java/com/android/component/explorer/scanner/DirExplorerTest.java
@@ -1,14 +1,43 @@
 package com.android.component.explorer.scanner;
 
+import com.android.component.explorer.manager.UnitManager;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by parkjaesung on 2017. 4. 10..
  */
 public class DirExplorerTest {
+
+    DirExplorer.FileHandler fileHandler;
+    DirExplorer.Filter filter;
+
+    UnitManager unitManager;
+    StringBuilder rootPath = new StringBuilder(System.getProperty("user.dir"));
+
+    @Before
+    public void setUp(){
+        fileHandler = FileHandler.getInstance();
+        filter = new Filter();
+        unitManager = UnitManager.getInstance();
+    }
     @Test
     public void explore() throws Exception {
+        DirExplorer dirExplorer = new DirExplorer(this.filter, this.fileHandler);
 
+        String samplePath = rootPath.append("/src/test/java/com/android/component/explorer/scanner/sample").toString();
+        dirExplorer.explore(new File(samplePath));
+
+        ArrayList<String> expectedActivityNames = new ArrayList<String>(Arrays.asList(
+                "ActivitySample"
+        ));
+        assertEquals(expectedActivityNames, new ArrayList<String>(unitManager.getActivities().keySet()));
     }
 
 }
