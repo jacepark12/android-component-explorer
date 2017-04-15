052b3a81eaa6ec5957f9e43b87a6dabed022d8d3
ParkJaesung <jspark141515@gmail.com>
2017-04-16 00:38:54 +0900

    Test case for FileHandler

---
+++ src/test/java/com/android/component/explorer/scanner/FileHandlerTest.java
@@ -0,0 +1,27 @@
+package com.android.component.explorer.scanner;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by parkjaesung on 2017. 4. 16..
+ */
+public class FileHandlerTest {
+
+    @Test
+    public void initializeTest(){
+        FileHandler fileHandler = FileHandler.getInstance();
+
+        Set<String> expected = new HashSet<String>();
+        expected.add("Activity");
+        expected.add("AppCompatActivity");
+
+        assertEquals(expected, fileHandler.getActivityClassNames());
+    }
+
+
+}
