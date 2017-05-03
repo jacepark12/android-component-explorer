236f6d2ab87e080591b5477224adaddc60217496
ParkJaesung <jspark141515@gmail.com>
2017-05-03 23:44:43 +0900

    Finished event handling on Jtree
    Removed unuseful comment lines

--- src/main/java/com/android/component/explorer/ToolWindowTest.java
+++ src/main/java/com/android/component/explorer/ToolWindowTest.java
@@ -1,42 +1,49 @@
 package com.android.component.explorer;
 
+import com.android.component.explorer.manager.UnitManager;
 import com.android.component.explorer.scanner.DirExplorer;
 import com.android.component.explorer.scanner.FileHandler;
 import com.android.component.explorer.scanner.Filter;
-import com.intellij.openapi.fileEditor.FileEditor;
+import com.android.component.explorer.unit.ActivityUnit;
+import com.android.component.explorer.unit.ComponentUnit;
+import com.android.component.explorer.unit.FragmentUnit;
 import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.FileEditorState;
-import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectRootManager;
-import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
-import com.intellij.ui.treeStructure.Tree;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeModel;
+import java.awt.event.*;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Iterator;
 
 /**
  * Created by parkjaesung on 2017. 4. 10..
  */
 public class ToolWindowTest implements ToolWindowFactory {
     private JButton button1;
-    private JPanel MainPanel;
+    private JPanel MainPanel2;
     private JLabel Label1;
     private JTree tree;
+
+    private HashMap<String, ActivityUnit> activityMap;
+    private HashMap<String, FragmentUnit> fragmentMap;
+
+    UnitManager unitManager = UnitManager.getInstance();
 
     public void createToolWindowContent(@NotNull final Project project, @NotNull ToolWindow toolWindow) {
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         System.out.println("creatToolWindow");
-        Content content = contentFactory.createContent(MainPanel, "",false);
+        Content content = contentFactory.createContent(MainPanel2, "",false);
         toolWindow.getContentManager().addContent(content);
 
         final VirtualFile[] vFiles = ProjectRootManager.getInstance(project).getContentRoots();
@@ -44,36 +51,74 @@ public class ToolWindowTest implements ToolWindowFactory {
         System.out.println("path : " + vFiles[0].getPath());
         System.out.println("canonicalpath : " + vFiles[0].getCanonicalPath());
 
+        final DefaultMutableTreeNode activityNode = new DefaultMutableTreeNode("Activity");
+        final DefaultMutableTreeNode fragmentNode = new DefaultMutableTreeNode("Fragment");
+
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode("Project");
+
+        root.add(activityNode);
+        root.add(fragmentNode);
+
+        TreeModel treeModel = new DefaultTreeModel(root);
+
+        tree.setModel(treeModel);
+        scanProject(vFiles[0].getCanonicalPath());
+
         button1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 Label1.setText("Button Clicked");
-                System.out.println("test");
                 scanProject(vFiles[0].getCanonicalPath());
 
-                //FileEditor[] editors = FileEditorManager.getInstance(project).openFile(vFiles[0], true);
-                new OpenFileDescriptor(project, vFiles[0]).navigate(true);
-                //System.out.println("FileEditor length : " + editors.length);
-                //editors[0].setState(FileEditorState.INSTANCE);
-                VirtualFile virtualFile = LocalFileSystem.getInstance().findFileByIoFile(new File("/Users/parkjaesung/Documents/workspace/Github/android-viewbinder/src/main/java/com/android/component/explorer/ToolWindowTest.java"));
-                FileEditor[] editors = FileEditorManager.getInstance(project).openFile(virtualFile, true);
-                editors[0].setState(FileEditorState.INSTANCE);
+                activityMap = unitManager.getActivities();
+                fragmentMap = unitManager.getFragments();
+
+                Iterator iterator = activityMap.keySet().iterator();
+                //Set Activity node
+                while(iterator.hasNext()){
+                    DefaultMutableTreeNode activitySubNode = new DefaultMutableTreeNode(activityMap.get(iterator.next()));
+                    activityNode.add(activitySubNode);
+                }
+
+                //Set Fragment node
+                iterator = fragmentMap.keySet().iterator();
+                while (iterator.hasNext()){
+                    DefaultMutableTreeNode fragmentSubNode = new DefaultMutableTreeNode(fragmentMap.get(iterator.next()));
+                    fragmentNode.add(fragmentSubNode);
+                }
             }
         });
 
-        //configure tree1
-        //create the root node
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode("Root");
-        //create the child nodes
-        DefaultMutableTreeNode vegetableNode = new DefaultMutableTreeNode("Vegetables");
-        DefaultMutableTreeNode fruitNode = new DefaultMutableTreeNode("Fruits");
-        //add the child nodes to the root node
-        root.add(vegetableNode);
-        root.add(fruitNode);
+        tree.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                 //if enter key has been pressed
+                if(e.getKeyCode()==10){
+                    DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
 
-        //create the tree by passing in the root node
-        tree = (Tree) new JTree(root);
-        MainPanel.add(tree);
+                    Object userObject = selectedNode.getUserObject();
 
+                    if(userObject instanceof ActivityUnit || userObject instanceof FragmentUnit){
+                        ComponentUnit componentUnit = (ComponentUnit)userObject;
+                        openFileInEditor(project, componentUnit.getVirtualFile());
+                    }
+                }
+            }
+        });
+        tree.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if(e.getClickCount() == 2){
+                    DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+
+                    Object userObject = selectedNode.getUserObject();
+
+                    if(userObject instanceof ActivityUnit || userObject instanceof FragmentUnit){
+                        ComponentUnit componentUnit = (ComponentUnit)userObject;
+                        openFileInEditor(project, componentUnit.getVirtualFile());
+                    }
+                }
+            }
+        });
     }
 
     private void scanProject(String rootDir){
@@ -83,5 +128,14 @@ public class ToolWindowTest implements ToolWindowFactory {
         DirExplorer dirExplorer = new DirExplorer(Filter.getInstance(), FileHandler.getInstance());
 
         dirExplorer.explore(file);
+
+        UnitManager unitManager = UnitManager.getInstance();
+
+        System.out.println(unitManager.getActivities().size());
+        System.out.println(unitManager.getFragments().size());
+    }
+
+    public void openFileInEditor(Project project, VirtualFile virtualFile){
+        FileEditorManager.getInstance(project).openFile(virtualFile, true);
     }
 }
