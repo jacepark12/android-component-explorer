846c82f19df127edcf3dd79c51b18e002d413ddc
ParkJaesung <jspark141515@gmail.com>
2017-05-08 13:47:51 +0900

    Added new constructor for LayoutUnit class

--- src/main/java/com/android/component/explorer/unit/LayoutUnit.java
+++ src/main/java/com/android/component/explorer/unit/LayoutUnit.java
@@ -1,7 +1,13 @@
 package com.android.component.explorer.unit;
 
+import com.intellij.openapi.vfs.VirtualFile;
+
 /**
  * Created by parkjaesung on 2017. 4. 13..
  */
 public class LayoutUnit extends ComponentUnit{
+
+    public LayoutUnit(String name, VirtualFile virtualFile){
+        super(name, virtualFile);
+    }
 }
