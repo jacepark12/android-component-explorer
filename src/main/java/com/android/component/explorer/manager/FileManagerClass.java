72eeab3c5139a710aa61cbc1064c635cfc362417
ParkJaesung <jspark141515@gmail.com>
2017-05-08 16:07:03 +0900

    Renamed class FileManagerClass saves File object with <String, File> HashMap

---
+++ src/main/java/com/android/component/explorer/manager/FileManagerClass.java
@@ -0,0 +1,41 @@
+package com.android.component.explorer.manager;
+
+import com.android.component.explorer.manager.exception.XmlLayoutNotFound;
+
+import java.io.File;
+import java.util.HashMap;
+
+/**
+ * Created by parkjaesung on 2017. 5. 8..
+ */
+public class FileManagerClass {
+
+    private static FileManagerClass instance;
+    private HashMap<String, File> xmlLayoutFile = new HashMap<String, File>();
+    private FileManagerClass(){
+
+    }
+
+    public static FileManagerClass getInstance(){
+        if(instance == null){
+            return instance = new FileManagerClass();
+        }
+        return instance;
+    }
+
+    public void addXmlLayoutFile(String name, File file){
+        this.xmlLayoutFile.put(name, file);
+    }
+
+    public HashMap<String, File> getXmlLayoutFile() {
+        return xmlLayoutFile;
+    }
+
+    public File getXmlLayoutFileByName(String name) throws XmlLayoutNotFound {
+        if(!this.xmlLayoutFile.containsKey(name)){
+            throw new XmlLayoutNotFound("No" + name + "XML layout found in project ");
+        }
+
+        return this.xmlLayoutFile.get(name);
+    }
+}
