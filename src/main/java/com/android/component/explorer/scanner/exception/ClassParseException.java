3840cd318f0ae82568df6764bcb62664d45380a6
ParkJaesung <jspark141515@gmail.com>
2017-05-08 13:48:14 +0900

    Added custom exception for ClassParseException

---
+++ src/main/java/com/android/component/explorer/scanner/exception/ClassParseException.java
@@ -0,0 +1,11 @@
+package com.android.component.explorer.scanner.exception;
+
+/**
+ * Created by parkjaesung on 2017. 5. 8..
+ */
+public class ClassParseException extends Exception {
+
+    public ClassParseException(String message){
+        super(message);
+    }
+}
