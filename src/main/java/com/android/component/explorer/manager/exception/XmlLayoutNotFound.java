c7a528571f10bb8bd403e9ba39c5e458a8400aa4
ParkJaesung <jspark141515@gmail.com>
2017-05-08 14:49:26 +0900

    Created XMLLayoutNotFound exception class

---
+++ src/main/java/com/android/component/explorer/manager/exception/XmlLayoutNotFound.java
@@ -0,0 +1,11 @@
+package com.android.component.explorer.manager.exception;
+
+/**
+ * Created by parkjaesung on 2017. 5. 8..
+ */
+public class XmlLayoutNotFound extends Exception {
+
+    public XmlLayoutNotFound(String message){
+        super(message);
+    }
+}
