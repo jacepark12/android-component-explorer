7f46056051ce5de197d06aeef4677718dc9dbf03
ParkJaesung <jspark141515@gmail.com>
2017-05-10 14:52:31 +0900

    Created ClassAndParentManager class

---
+++ src/main/java/com/android/component/explorer/manager/ClassAndParentManager.java
@@ -0,0 +1,33 @@
+package com.android.component.explorer.manager;
+
+import java.util.HashMap;
+
+/**
+ * Created by parkjaesung on 2017. 5. 10..
+ */
+public class ClassAndParentManager {
+
+    private HashMap<String, String> classAndParent = new HashMap<String, String>();
+
+    private static ClassAndParentManager instance;
+
+    private ClassAndParentManager(){
+
+    }
+
+    public static ClassAndParentManager getInstance(){
+        if(instance == null){
+            return instance = new ClassAndParentManager();
+        }
+        return instance;
+    }
+
+    public void setClassAndParent(String child, String parent){
+        this.classAndParent.put(child, parent);
+    }
+
+    public String getParentPackageByChild(String child){
+        return this.classAndParent.get(child);
+    }
+    9
+}
